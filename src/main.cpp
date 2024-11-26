2b04122e1caadefcb0df7fb9d11526a180788309
Matthew-K-Chua <mattkchuas@gmail.com>
2024-11-27 10:14:58 +1100

    Working board output for 2d array

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,11 @@
-/*********************************************************************
-  This is an example for our Monochrome OLEDs based on SH110X drivers
-
-  This example is for a 128x64 size display using SPi to communicate
-  5 pins are required to interface 
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada  for Adafruit Industries.
-  BSD license, check license.txt for more information
-  All text above, and the splash screen must be included in any redistribution
-
-  SPi SH1106 modified by Rupert Hirst  12/09/21
-*********************************************************************/
-
-
+// Libraries
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 
-
-// #define OLED_MOSI     10
-// #define OLED_CLK      8
-// #define OLED_DC       7
-// #define OLED_CS       5
-// #define OLED_RST      9
-
+// OLED pin definitions
 #define OLED_CLK      23
 #define OLED_MOSI     22
 #define OLED_RST      1
@@ -43,7 +20,6 @@ Adafruit_SH1106G display = Adafruit_SH1106G(128, 64,OLED_MOSI, OLED_CLK, OLED_DC
 #define XPOS 0
 #define YPOS 1
 #define DELTAY 2
-
 
 #define LOGO16_GLCD_HEIGHT 16
 #define LOGO16_GLCD_WIDTH  16
@@ -66,294 +42,96 @@ static const unsigned char PROGMEM logo16_glcd_bmp[] =
   B00000000, B00110000
 };
 
-void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-  uint8_t icons[NUMFLAKES][3];
+int board[3][3] = {{1, -1, 1}, {1, 1, 0}, {-1, 0, -1}};
 
-  // initialize
-  for (uint8_t f = 0; f < NUMFLAKES; f++) {
-    icons[f][XPOS] = random(display.width());
-    icons[f][YPOS] = 0;
-    icons[f][DELTAY] = random(5) + 1;
+void displayExample() {
+  display.clearDisplay();
+  String stringOne   = " X | O | X ";
+  String stringTwo   = " X | O | X ";
+  String stringThree = " O | X | O ";
 
-    Serial.print("x: ");
-    Serial.print(icons[f][XPOS], DEC);
-    Serial.print(" y: ");
-    Serial.print(icons[f][YPOS], DEC);
-    Serial.print(" dy: ");
-    Serial.println(icons[f][DELTAY], DEC);
-  }
+  display.setTextSize(1); // Set text size
+  display.setTextColor(SH110X_WHITE); // Set text color
 
-  while (1) {
-    // draw each icon
-    for (uint8_t f = 0; f < NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SH110X_WHITE);
-    }
-    display.display();
-    delay(200);
+  display.setCursor(0, 0); // Set cursor position
+  display.println(stringOne); // Print message
 
-    // then erase it + move it
-    for (uint8_t f = 0; f < NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SH110X_BLACK);
-      // move it
-      icons[f][YPOS] += icons[f][DELTAY];
-      // if its gone, reinit
-      if (icons[f][YPOS] > display.height()) {
-        icons[f][XPOS] = random(display.width());
-        icons[f][YPOS] = 0;
-        icons[f][DELTAY] = random(5) + 1;
+  display.setCursor(0, 10); // Set cursor position
+  display.println(stringTwo); // Print message
+
+  display.setCursor(0, 20); // Set cursor position
+  display.println(stringThree); // Print message
+
+  display.display(); // Update the display with the buffer content
+}
+
+void displayBoard() {
+  display.clearDisplay();
+  display.setTextSize(1); // Set text size
+  display.setTextColor(SH110X_WHITE); // Set text color
+  for (int row=0;row<3;row++) { //rows
+    String rowString = "";
+    for (int column=0;column<3;column++) { //columns
+      String colString = "";
+      switch (board[row][column]) {
+        case 0:
+          colString = "   ";
+          rowString = rowString + colString;
+          Serial.print(colString);
+          break;
+        case 1:
+          colString = " X ";
+          rowString = rowString + colString;
+          Serial.print(colString);
+          break;
+        case -1:
+          colString = " O ";
+          rowString = rowString + colString;
+          Serial.print(colString);
+          break;
       }
-    }
+      if (column < 2) {
+        rowString = rowString + "|";
+        Serial.print("|");
+      }
+    } 
+    display.setCursor(0, row*10); // Set cursor position
+    display.println(rowString);
+    Serial.println();
   }
+  display.display(); // Update the display with the buffer content
+  Serial.println();
+  Serial.println("--------------------");
 }
-
-
-void testdrawchar(void) {
-  display.setTextSize(1);
-  display.setTextColor(SH110X_WHITE);
-  display.setCursor(0, 0);
-
-  for (uint8_t i = 0; i < 168; i++) {
-    if (i == '\n') continue;
-    display.write(i);
-    if ((i > 0) && (i % 21 == 0))
-      display.println();
-  }
-  display.display();
-  delay(1);
-}
-
-void testdrawcircle(void) {
-  for (int16_t i = 0; i < display.height(); i += 2) {
-    display.drawCircle(display.width() / 2, display.height() / 2, i, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testfillrect(void) {
-  uint8_t color = 1;
-  for (int16_t i = 0; i < display.height() / 2; i += 3) {
-    // alternate colors
-    display.fillRect(i, i, display.width() - i * 2, display.height() - i * 2, color % 2);
-    display.display();
-    delay(1);
-    color++;
-  }
-}
-
-void testdrawtriangle(void) {
-  for (int16_t i = 0; i < min(display.width(), display.height()) / 2; i += 5) {
-    display.drawTriangle(display.width() / 2, display.height() / 2 - i,
-                         display.width() / 2 - i, display.height() / 2 + i,
-                         display.width() / 2 + i, display.height() / 2 + i, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testfilltriangle(void) {
-  uint8_t color = SH110X_WHITE;
-  for (int16_t i = min(display.width(), display.height()) / 2; i > 0; i -= 5) {
-    display.fillTriangle(display.width() / 2, display.height() / 2 - i,
-                         display.width() / 2 - i, display.height() / 2 + i,
-                         display.width() / 2 + i, display.height() / 2 + i, SH110X_WHITE);
-    if (color == SH110X_WHITE) color = SH110X_BLACK;
-    else color = SH110X_WHITE;
-    display.display();
-    delay(1);
-  }
-}
-
-void testdrawroundrect(void) {
-  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2) {
-    display.drawRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i, display.height() / 4, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testfillroundrect(void) {
-  uint8_t color = SH110X_WHITE;
-  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2) {
-    display.fillRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i, display.height() / 4, color);
-    if (color == SH110X_WHITE) color = SH110X_BLACK;
-    else color = SH110X_WHITE;
-    display.display();
-    delay(1);
-  }
-}
-
-void testdrawrect(void) {
-  for (int16_t i = 0; i < display.height() / 2; i += 2) {
-    display.drawRect(i, i, display.width() - 2 * i, display.height() - 2 * i, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-}
-
-void testdrawline() {
-  for (int16_t i = 0; i < display.width(); i += 4) {
-    display.drawLine(0, 0, i, display.height() - 1, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i = 0; i < display.height(); i += 4) {
-    display.drawLine(0, 0, display.width() - 1, i, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i = 0; i < display.width(); i += 4) {
-    display.drawLine(0, display.height() - 1, i, 0, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i = display.height() - 1; i >= 0; i -= 4) {
-    display.drawLine(0, display.height() - 1, display.width() - 1, i, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i = display.width() - 1; i >= 0; i -= 4) {
-    display.drawLine(display.width() - 1, display.height() - 1, i, 0, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i = display.height() - 1; i >= 0; i -= 4) {
-    display.drawLine(display.width() - 1, display.height() - 1, 0, i, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-  for (int16_t i = 0; i < display.height(); i += 4) {
-    display.drawLine(display.width() - 1, 0, 0, i, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (int16_t i = 0; i < display.width(); i += 4) {
-    display.drawLine(display.width() - 1, 0, i, display.height() - 1, SH110X_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-}
-
 
 void setup()   {
-  Serial.begin(9600);
-
-  //display.setContrast (0); // dim display
+  Serial.begin(9600); // Initialise Serial for later debugging
+  delay(100);
+  Serial.println("PLZ WORK");
+  delay(1000);
 
   // Start OLED
   display.begin(0, true); // we dont use the i2c address but we will reset!
-  // display.setRotation();
 
-  display.setRotation(2); // Set rotation to 180 degrees
+  display.setRotation(2); // Ensures the display is not upside down
   display.clearDisplay(); // Clear the display buffer
+
   display.setTextSize(1); // Set text size
   display.setTextColor(SH110X_WHITE); // Set text color
   display.setCursor(0, 0); // Set cursor position
   display.println("Hello, World!"); // Print message
   display.display(); // Update the display with the buffer content
-  delay(100000);
+  display.clearDisplay(); // Clear the display buffer
 
-  // Show image buffer on the display hardware.
-  // Since the buffer is intialized with an Adafruit splashscreen
-  // internally, this will display the splashscreen.
-  // display.display();
-  // delay(2000);
-
-  // Clear the buffer.
-  display.clearDisplay();
-
-  // draw a single pixel
-  display.drawPixel(10, 10, SH110X_WHITE);
-  // Show the display buffer on the hardware.
-  // NOTE: You _must_ call display after making any drawing commands
-  // to make them visible on the display hardware!
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw many lines
-  testdrawline();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw rectangles
-  testdrawrect();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw multiple rectangles
-  testfillrect();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw mulitple circles
-  testdrawcircle();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw a SH110X_WHITE circle, 10 pixel radius
-  display.fillCircle(display.width() / 2, display.height() / 2, 10, SH110X_WHITE);
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  testdrawroundrect();
-  delay(2000);
-  display.clearDisplay();
-
-  testfillroundrect();
-  delay(2000);
-  display.clearDisplay();
-
-  testdrawtriangle();
-  delay(2000);
-  display.clearDisplay();
-
-  testfilltriangle();
-  delay(2000);
-  display.clearDisplay();
-
-  // draw the first ~12 characters in the font
-  testdrawchar();
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-
-  // text display tests
-
-
-  // miniature bitmap display
-  display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
-  display.display();
-  delay(1);
-
-  // invert the display
-  display.invertDisplay(true);
   delay(1000);
-  display.invertDisplay(false);
-  delay(1000);
-  display.clearDisplay();
-
-  // draw a bitmap icon and 'animate' movement
-  testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
 }
 
 
 void loop() {
-
+  displayExample();
+  delay(2000);
+  displayBoard();
+  delay(2000);
+  Serial.println("Hello Worldddd");
 }
+
